5566d129982d61808f6a3b26cd55ccab7f714c10
Maxence Habar <50749092+Maxencehabar@users.noreply.github.com>
2024-02-25 09:40:32 +0100

    fix: chatGPT response error

--- chatGPT/newChatGPT.py
+++ chatGPT/newChatGPT.py
@@ -1,35 +1,57 @@
 import os
 import requests
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 headers = {
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer ' + os.getenv('OPEN_AI_API_KEY'),
+    "Content-Type": "application/json",
+    "Authorization": "Bearer " + os.getenv("OPEN_AI_API_KEY"),
 }
 
 with open("chatGPT/preprompt.txt", "r") as f:
     preprompt = f.read()
 
+
 def Analyse(match, stats):
     json_data = {
-        'model': 'gpt-3.5-turbo',
-        'messages': [
+        "model": "gpt-4",
+        "messages": [
             {
-                'role': 'system',
-                'content': preprompt,
+                "role": "system",
+                "content": preprompt,
             },
             {
-                'role': 'user',
-                'content': f"{match} Here are the stats : {stats}. Now analyse the data.",
+                "role": "user",
+                "content": f"{match} Here are the stats : {stats}. Now analyse the data.",
             },
         ],
     }
 
-    response = requests.post('https://api.openai.com/v1/chat/completions', headers=headers, json=json_data)
+    response = requests.post(
+        "https://api.openai.com/v1/chat/completions", headers=headers, json=json_data
+    )
 
-    #print(response.json())
-    res = response.json()["choices"][0]["message"]["content"]
-    print(res)
-    return res
+    # print(response.json())
+    logging.info(response.status_code)
+
+    if response.status_code == 429:
+        logging.error("Rate limit exceeded")
+        return "Rate limit exceeded"
+
+    if response.status_code != 200:
+        logging.error(response.json())
+
+        return "Server error : status code " + str(response.status_code)
+    try:
+        res = response.json()["choices"][0]["message"]["content"]
+        print(res)
+        return res
+    except Exception as e:
+        logging.error(response.json())
+        logging.error("Can't access response")
+        return e
+
 
 if __name__ == "__main__":
     Analyse(match="Here are the stats", stats="salut")
